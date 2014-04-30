3c542f61103af460d1d4ca3758c307342aa2e6a9
CCrosbyweb <Celeste@eagles.ewu.edu>
2014-04-30 14:40:20 -0700

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
 // My Scripts
 $(document).foundation();
+//animate.css stuff
+
+$('h3').click(function(){
+	
+	$('h3').addClass('animated rubberBand');
+	console.log('click me')
+});
+
+//transit.js stuff
+$('img').click(function(){
+	
+	//Do Stuff
+	$("p").transition({
+		x: 100, 
+		y: 100,
+	});
+
+});
